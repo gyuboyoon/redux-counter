b82dcadc087641bc7c7794614a33ac3fce2f6054
gyubo <yoon9yubo@gmail.com>
2025-02-19 00:53:15 +0900

    여러개의 slice 관리

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import { createStore } from "redux";
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 import { act } from "react";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState: initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -54,11 +54,29 @@ const counterSlice = createSlice({
 
 //   return state;
 // };
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.initialAuthState = false;
+    },
+  },
+});
 
 // const store = createStore(counterReducer);
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
