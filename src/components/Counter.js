0b213c252883c467f650b4a2948e3b4e25ffd6a4
gyubo <yoon9yubo@gmail.com>
2025-02-19 01:41:29 +0900

    redux 분리

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,7 @@
 // import { Component } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
-import { counterActions } from "../store/index.js";
+import { counterActions } from "../store/counter";
 
 import classes from "./Counter.module.css";
 
---
+++ src/store/auth.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false; // 수정된 부분
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+export default authSlice.reducer;
